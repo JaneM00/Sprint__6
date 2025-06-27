5134a931eb2269ec9b8f1b0481c84a4af0dd3578
JaneM00 <evg.mamikonyan@yandex.ru>
2025-06-27 12:37:55 +0300

    Update test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -4,23 +4,22 @@ import pytest
 from pages.main_page import MainPage
 from pages.order_page import OrderPage
 
-@pytest.mark.parametrize("name,surname,address,phone,order_button_type", [
-    ("Иван", "Иванов", "ул. Ленина д.1", "+79991112233", "top"),
-    ("Петр", "Петров", "пр. Мира д.10", "+79994445566", "bottom")
+# Определите локаторы для кнопок заказа
+ORDER_BUTTON_TOP = (By.CSS_SELECTOR, ".order-top")     # замените на актуальный локатор
+ORDER_BUTTON_BOTTOM = (By.CSS_SELECTOR, ".order-bottom")  # замените на актуальный локатор
+
+@pytest.mark.parametrize("name,surname,address,phone,order_button_locator", [
+    ("Иван", "Иванов", "ул. Ленина д.1", "+79991112233", ORDER_BUTTON_TOP),
+    ("Петр", "Петров", "пр. Мира д.10", "+79994445566", ORDER_BUTTON_BOTTOM)
 ])
-def test_order_flow(driver, name, surname, address, phone, order_button_type):
+def test_order_flow(driver, name, surname, address, phone, order_button_locator):
     page = MainPage(driver)
     page.open()
     
-    # Выбираем кнопку для заказа на основе параметра
-    if order_button_type == "top":
-        page.click_order_top()
-    elif order_button_type == "bottom":
-        page.click_order_bottom()
-    else:
-        pytest.fail(f"Неизвестный тип кнопки заказа: {order_button_type}")
-    
     order_page = OrderPage(driver)
+    
+    # Кликаем по нужной кнопке заказа
+    order_page.click_order_button(order_button_locator)
     
     order_page.fill_order_form(name, surname, address, phone)
     order_page.submit_order()
