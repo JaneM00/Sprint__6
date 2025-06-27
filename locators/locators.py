c888599bd9b8212c23b945003252afb3dba80f8c
JaneM00 <evg.mamikonyan@yandex.ru>
2025-06-27 12:46:26 +0300

    Update locators.py

--- locators/locators.py
+++ locators/locators.py
@@ -1,5 +1,4 @@
 # locators/locators.py
-
 from selenium.webdriver.common.by import By
 
 class MainPageLocators:
@@ -24,23 +23,23 @@ class MainPageLocators:
         "question7": (By.CSS_SELECTOR, "[data-answer='question7']"),
         "question8": (By.CSS_SELECTOR, "[data-answer='question8']")
     }
-    # Локатор для логотипа
+    # Логотип
     LOGO = (By.CSS_SELECTOR, ".header-logo")  
 
 class OrderPageLocators:
-    # Локаторы для формы заказа
+    # Локаторы формы заказа
     NAME_INPUT = (By.NAME, "name")
     SURNAME_INPUT = (By.NAME, "surname")
     ADDRESS_INPUT = (By.NAME, "address")
     PHONE_INPUT = (By.NAME, "phone")
-    SUBMIT_BUTTON = (By.XPATH, "//button[contains(text(), 'Заказать')]")  
+    SUBMIT_BUTTON = (By.XPATH, "//button[contains(text(), 'Заказать')]") 
 
 class MainPageHeaderLocators:
     # Локатор для кнопки заказа сверху
-    ORDER_TOP_BUTTON = (By.CSS_SELECTOR, ".Order_Button_Top")  
+    ORDER_TOP_BUTTON = (By.CSS_SELECTOR, ".order-top")  
     # Локатор для кнопки заказа снизу
-    ORDER_BOTTOM_BUTTON = (By.CSS_SELECTOR, ".Order_Button_Bottom")  
+    ORDER_BOTTOM_BUTTON = (By.CSS_SELECTOR, ".order-bottom")  
 
 class ConfirmationLocators:
-    # Локатор для сообщения об успешном заказе
+    # Локатор сообщения об успешном заказе
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".Order_Modal__Title")  
